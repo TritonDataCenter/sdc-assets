89c12a7ab49cb56270304055319b5bbbbc8091eb
Josh Wilsdon <jwilsdon@joyent.com>
2013-08-28 18:33:24 -0700

    HEAD-1804 add setup + configure + submodule to assets zone to support moving to in-zone scripts

--- package.sh
+++ package.sh
@@ -10,7 +10,15 @@ ROOT=$(pwd)
 
 tmpdir="/tmp/assets.$$"
 mkdir -p ${tmpdir}/root/assets
+mkdir -p ${tmpdir}/root/opt/smartdc/sdc-boot/scripts
 mkdir -p ${tmpdir}/site
+
+# update/create sdc-scripts
+git submodule update --init ${ROOT}/deps/sdc-scripts
+
+# copy in sdc-boot scripts
+cp -r ${ROOT}/sdc-boot/* ${tmpdir}/root/opt/smartdc/sdc-boot/
+cp -r ${ROOT}/deps/sdc-scripts/* ${tmpdir}/root/opt/smartdc/sdc-boot/scripts/
 
 ( cd ${tmpdir}; tar -jcf ${ROOT}/${RELEASE_TARBALL} root site)
 
